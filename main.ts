b065f7f661da4b67a6518903cced04c3c7ab2baa
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-02-26 17:19:44 -0500

    programmed to turn LED on when A is pressed

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Noah
+ * Created on: Feb 2026
+ * This program turns an LED on when A is pressed
 */
 
-basic.showString('Hello, World!')
+// the setup
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+// turning on LED
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turning off LED
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
