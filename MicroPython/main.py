c4e6404d229ab8ab662c5a942b8468fa509f633d
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-02-27 18:08:46 +0000

    created python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,4 +6,15 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.clear()
+pin16.write_digital(0)
+display.show(Image.HAPPY)
+
+# on  and off
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
